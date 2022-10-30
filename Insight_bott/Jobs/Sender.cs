a8bed7018dea6f3f2f4393e7b6021d08940e8d5d
0z2 <67888219+0z2@users.noreply.github.com>
2022-10-30 21:27:24 +0500

    Initial commit

---
+++ Insight_bott/Jobs/Sender.cs
@@ -0,0 +1,73 @@
+﻿using Quartz;
+using System.Net;
+using System.Net.Mail;
+using System.Threading.Tasks;
+using Telegram.Bot;
+using Telegram.Bot.Types;
+
+namespace Insight_bott.Jobs
+{
+    public class Sender : IJob
+    {
+        Users users;
+        TelegramBotClient Client;
+
+        public Sender()
+        {
+            users = new Users();
+            Client = new TelegramBotClient("5465942614:AAHkdHQIxHrfHOboa3TlzV7gPHHYq00vdqA");
+            Client.StartReceiving(Update, Error);
+        }
+
+        
+        public async Task Execute(IJobExecutionContext context)
+        {
+            Message message = await Client.SendTextMessageAsync(
+            chatId: 985485455,
+            text: "Бот не упущен!");
+        }
+        static Task Error(ITelegramBotClient arg1, Exception arg2, CancellationToken arg3)
+        {
+            throw new NotImplementedException();
+        }
+
+        async Task Update(ITelegramBotClient botClient, Update update, CancellationToken token)
+        {
+            var message = update.Message;
+            if (message.Text != null)
+            {
+                Console.WriteLine($" {message.Chat.Id}");
+                if (message.Text.ToLower().Contains("здорова"))
+                {
+                    await botClient.SendTextMessageAsync(message.Chat.Id, "Здоровей видали");
+                    //await botClient.SendTextMessageAsync(message.Chat.Id, user.GetCurrentThought()); //это как мысль отправить
+                    return;
+                }
+                else if (message.Text.ToLower() == "/start")
+                {
+                    long userTelegramId = message.Chat.Id;
+                    Insight_bott.User newUser = new Insight_bott.User(userTelegramId); // добавить проверку на случай, если пользователь уже есть в базе
+                    users.ListOfUsers.Add(newUser);
+                    await botClient.SendTextMessageAsync(message.Chat.Id, "Добро пожаловать!");
+                    return;
+                }
+                else if (message.Text.ToLower() == "/get_thought")
+                {
+                    Insight_bott.User currentUser = users.ReturnUser(message.Chat.Id);
+                    if (currentUser == null) // заплатка на случай если пользователя нет в списке пользователей, но он отправил сообщение
+                    {
+
+                    }
+                    else
+                    {
+                        string currentUserThought = currentUser.GetCurrentThought();
+                        await botClient.SendTextMessageAsync(message.Chat.Id, currentUserThought);
+                    }
+
+                }
+            }
+        }
+    }
+
+
+}
---
+++ Insight_bott/Program.cs
@@ -0,0 +1,12 @@
+﻿using Insight_bott;
+using Quartz.Impl;
+using Quartz;
+using System.Threading;
+using Telegram.Bot;
+using Telegram.Bot.Types;
+
+Insight_bott.Jobs.Sheduler sheduler = new Insight_bott.Jobs.Sheduler();
+sheduler.Start();
+
+
+Console.ReadLine();
