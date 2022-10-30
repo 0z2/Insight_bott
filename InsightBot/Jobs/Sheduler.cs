e8f9458cd9cb172ecff6abb9fcefe686600aebb4
0z2 <67888219+0z2@users.noreply.github.com>
2022-10-31 03:09:57 +0500

    initial commit

---
+++ InsightBot/Jobs/Sheduler.cs
@@ -0,0 +1,32 @@
+﻿using Quartz.Impl;
+using Quartz;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Insight_bott.Jobs
+{
+    public class Sheduler
+    {
+        public async void Start()
+        {
+            
+            IScheduler scheduler = await StdSchedulerFactory.GetDefaultScheduler();
+            await scheduler.Start();
+
+            IJobDetail job = JobBuilder.Create<Sender>().Build();
+
+            ITrigger trigger = TriggerBuilder.Create()  // создаем триггер
+                .WithIdentity("trigger1", "group1")     // идентифицируем триггер с именем и группой
+                .StartNow()                            // запуск сразу после начала выполнения
+                .WithSimpleSchedule(x => x            // настраиваем выполнение действия
+                    .WithIntervalInMinutes(1)         // через 1 минуту
+                    .RepeatForever())                   // бесконечное повторение
+                .Build();
+
+            await scheduler.ScheduleJob(job, trigger);        // начинаем выполнение работы
+        }
+    }
+}
