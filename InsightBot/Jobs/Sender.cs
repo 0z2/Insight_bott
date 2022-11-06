eeebf078e7420f64b7802d90b00f7bc791735a31
0z2 <67888219+0z2@users.noreply.github.com>
2022-11-07 00:09:37 +0500

    Мелкие правки логики

--- InsightBot/Jobs/Sender.cs
+++ InsightBot/Jobs/Sender.cs
@@ -23,13 +23,12 @@ namespace Insight_bott.Jobs
             Client = new TelegramBotClient(telegramBotApiKey);
             Client.StartReceiving(Update, Error);
         }
-
         
         public async Task Execute(IJobExecutionContext context)
         {
             Message message = await Client.SendTextMessageAsync(
             chatId: 985485455,
-            text: "Бот не упущен!");
+            text: "Бот запущен!");
         }
         static Task Error(ITelegramBotClient arg1, Exception arg2, CancellationToken arg3)
         {
@@ -39,51 +38,50 @@ namespace Insight_bott.Jobs
         async Task Update(ITelegramBotClient botClient, Update update, CancellationToken token)
         {
             var message = update.Message;
+            var currentUserTgId = message.Chat.Id;
+            
             if (message.Text != null)
             {
                 Console.WriteLine($" {message.Chat.Id}");
                 if (message.Text.ToLower().Contains("здорова"))
                 {
                     await botClient.SendTextMessageAsync(message.Chat.Id, "Здоровей видали");
-                    //await botClient.SendTextMessageAsync(message.Chat.Id, user.GetCurrentThought()); //это как мысль отправить
-                    return;
                 }
                 else if (message.Text.ToLower() == "/start")
                 {
-                    var userTelegramId = message.Chat.Id; // id того кто запросил мысль
                     var isAlreadyInBase = false;
                     
-                    // работаем с новым пользователем
-                    await using (ApplicationContext db = new ApplicationContext())
+                    // работаем с пользователем нажавшим start
+                    await using (ApplicationContext db = new ApplicationContext()) //подключаемся к контексту БД
                     {
                         // получаем список пользователей из БД
                         var users = db.Users.ToList();
                         // проверяем есть ли пользователь уже в базе
                         foreach (User u in users)
                         {
-                            if (u.Id == userTelegramId)
+                            if (u.Id == currentUserTgId)
                             {
                                 isAlreadyInBase = true;
+                                await botClient.SendTextMessageAsync(message.Chat.Id, "Вы уже есть в списке пользователей.");
                             }
                         }
                         //если пользователя нет в базе, тогда добавляем
                         if (isAlreadyInBase == false)
                         {
-                            Insight_bott.User newUser = new Insight_bott.User(userTelegramId);
-                            db.Users.AddRange(newUser);
-                            await db.SaveChangesAsync(token);
+                            Insight_bott.User newUser = new Insight_bott.User(currentUserTgId);
+                            db.Users.AddRange(newUser); // добавляем в таблицу нового пользователя
+                            await botClient.SendTextMessageAsync(message.Chat.Id, "Вы были добавлены в список пользователей.");
+                            // сохраняем изменения в таблице
+                            await db.SaveChangesAsync(token); // разобраться что это за токен такой и для чего он нужен
                         }
+
                     }
                     
-                    
-                    await botClient.SendTextMessageAsync(message.Chat.Id, "Добро пожаловать!");
-                    return;
                 }
                 
                 else if (message.Text.ToLower() == "/get_thought")
                 {
-                    var currentUserTgId = message.Chat.Id;
-                    User currentUser = null; //юзер который запросил мысль
+                    User currentUserFromDb = null; //юзер который запросил мысль
                     
                     // получение данных
                     await using (ApplicationContext db = new ApplicationContext())
@@ -96,28 +94,29 @@ namespace Insight_bott.Jobs
                             Console.WriteLine($"{u.Id} - при нажатии /get_thought");
                         }
                         
+                        //находим юзера, который запросил мысль
                         foreach (var user in users)
                         {
                             if (user.Id == currentUserTgId)
                             {
-                                currentUser = user;
+                                currentUserFromDb = user;
                             }
                         }
 
-                        if (currentUser == null) // заплатка на случай если пользователя нет в списке пользователей, но он отправил сообщение
+                        if (currentUserFromDb == null) // заплатка на случай если пользователя нет в списке пользователей, но он отправил сообщение
                         {
-
+                            Message message2 = await Client.SendTextMessageAsync(
+                                chatId: 985485455,
+                                text: $"Пользователь которого нет в базе запросил мыль. tgId пользователя: {currentUserTgId}");
                         }
                         else
                         {
-                            string currentUserThought = currentUser.GetCurrentThought();
+                            string currentUserThought = currentUserFromDb.GetCurrentThought();
                             await botClient.SendTextMessageAsync(message.Chat.Id, currentUserThought);
                         }
 
                     }
                 }
-
-
             }
         }
     }
